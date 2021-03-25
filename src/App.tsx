180917c96d1f69b375abc2ad46ce0304ba6aa28c
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-03-25 02:50:10 -0300

    estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle  from './styles/global';
 import {AuthProvider} from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
@@ -11,6 +12,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
 
     <GlobalStyle />
   </>
